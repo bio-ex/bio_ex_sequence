dddb328604b8fa7878fbeb5a75548c6500134ac4
Philip Capel <pcapel@users.noreply.github.com>
2023-08-02 15:08:38 -0500

    chore: update mix namespace

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule Bio.MixProject do
+defmodule Bio.Sequence.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/bio-ex/bio_ex_sequence"
 
   def project do
